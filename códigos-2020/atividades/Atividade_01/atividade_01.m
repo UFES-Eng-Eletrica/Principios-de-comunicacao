3c31cc66feb1cf6baa143771c49bda3646dcdfee
Arthur Lorencini Bergamaschi <arthur.lorencini@gmail.com>
2020-10-21 22:50:50 -0300

    adding more files

---
+++ códigos-2020/atividades/Atividade_01/atividade_01.m
@@ -0,0 +1,7 @@
+% Atividade feita por Arthur Lorencini Bergamaschi
+
+%%
+% 1 - Gere  um  vetor  tempo  com  duração  de  1μs  e  1000  posições  e
+% determine  o  período  de amostragem nesta situação.
+
+% Conforme visto na introdução ao matlab:
